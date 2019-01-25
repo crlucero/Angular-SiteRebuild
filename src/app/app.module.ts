736de27d92237528c52431aba6dc17b2a5df6acb
Cristian Lucero <cristianjlucero32@gmail.com>
2019-01-25 13:02:23 -0800

    This commit adds the component for the team slide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,

2cc425068707028bfcaa9a3af389a9cb000d62fe
Cristian Lucero <cristianjlucero32@gmail.com>
2019-01-25 14:01:06 -0800

    This commit takes out the broken nav bar and puts in the initial home-slide comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TeamComponent } from './team/team.component';
+import { HomeSlideComponent } from './home-slide/home-slide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    TeamComponent
+    TeamComponent,
+    HomeSlideComponent
   ],
   imports: [
     BrowserModule,

6cfe1516f52f812a0ceabcf17d52fd9321126fc4
Cristian Lucero <cristianjlucero32@gmail.com>
2019-01-31 13:47:19 -0800

    Succesfully installed firebase, started building blogs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TeamComponent } from './team/team.component';
 import { HomeSlideComponent } from './home-slide/home-slide.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     TeamComponent,
-    HomeSlideComponent
+    HomeSlideComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,

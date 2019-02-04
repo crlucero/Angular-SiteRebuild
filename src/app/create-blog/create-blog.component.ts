1f804281a0528bef3d556bfadc5f7db7ae190ecd
Cristian Lucero <cristianjlucero32@gmail.com>
2019-02-04 12:35:40 -0800

    This commit adds styling to blog detail page.

--- src/app/create-blog/create-blog.component.ts
+++ src/app/create-blog/create-blog.component.ts
@@ -1,25 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { BlogService } from "../blog.service";
 import { Blog } from "../blog.model";
 
 @Component({
-  selector: 'app-create-blog',
-  templateUrl: './create-blog.component.html',
-  styleUrls: ['./create-blog.component.css'],
+  selector: "app-create-blog",
+  templateUrl: "./create-blog.component.html",
+  styleUrls: ["./create-blog.component.css"],
   providers: [BlogService]
 })
 export class CreateBlogComponent implements OnInit {
+  constructor(private blogService: BlogService) {}
 
-  constructor(private blogService: BlogService) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
   // Submits form to create a new blog
   submitForm(title: string, name: string, description: string) {
     let newBlog = new Blog(title, name, description);
     this.blogService.addBlog(newBlog);
-    console.log(newBlog);
-    
+    // console.log(newBlog);
   }
-
 }
